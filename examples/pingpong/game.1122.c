218cfe6b1290d772fe2aed00c638826cd49bf4bb
Héctor J. Vásquez <ipi.vasquez@gmail.com>
2018-11-30 23:59:01 -0600

    ready!

--- examples/pingpong/game.1122.c
+++ examples/pingpong/game.1122.c
@@ -1,16 +1,15 @@
 #include <avr/interrupt.h>
 #include <avr/io.h>
 
-int inc = 0;         // Determines where to shift LEDs
-int scoreA = 0;      // Player A score (0-3)...
-int scoreB = 0;      // Player B score (0-3)...
-int speed;
+int inc = 0;    // Determina hacia donde recorrer LEDs
+int scoreA = 0; // Jugador A score (0-3)...
+int scoreB = 0; // Jugador B score (0-3)...
 
 /**
- * Resets all.
- * @param 1 if player A won, -1 if player B did.
+ * Reset.
+ * @param 1 si jugador A gana, -1 jugador B.
  */
-void celebration(int winner) {
+void resetPort(int winner) {
   // Reset
   scoreA = 0;
   scoreB = 0;
@@ -18,78 +17,85 @@ void celebration(int winner) {
 }
 
 int main() {
-  // Specifying outputs
-  DDRC = 0x3F; // Gaming LEDs: 32, 33, ..., 37
-  DDRA = 0x0F; // Score LEDs: 25, 24, 23, 22
+  // Especifica salidas
+  // LEDs del juego: 32, 33, ..., 37
+  DDRC |= 1 << PC5 | 1 << PC4 | 1 << PC3 | 1 << PC2 | 1 << PC1 | 1 << PC0;
+  // LEDs del marcador: 25, 24, 23, 22
+  DDRA |= 1 << PA0 | 1 << PA1 | 1 << PA2 | 1 << PA3;
   {
-    // Selecting interruption activation type and initialize interruptions
-    EICRA |= 0x0F; // Down to up
-    EIMSK |= 0x03; // External interruption 0 & 1
-    // Timer interruptions
-    TIMSK1 |= 1 << OCIE1A; // Interrupt on cmp A = true
-    // OCR1AH =0x1; // Number to compare | (first 8 bits)
-    // OCR1AL = 0; // Number to compare | (last 8 bits)
-    OCR1A = 0x008;
-    TCCR1A |= 0x00;
-    TCCR1B |= 1 << CS11 | 1 << CS10;
-    TCCR1C |= 0x80;
-    // ADC interruptions
+    // Interrupciones externas
+    EICRA |= 0x0F; // Flanco de subida
+    EIMSK |= 0x03; // Interrupciones externas 0 & 1
+    // Interruciones del Timer
+    TCCR1B |= 1 << WGM12;            // CTC
+    TCCR1B |= 1 << CS12 | 1 << CS10; // Prescalador 1024
+    TIMSK1 |= 1 << OCIE1A;           // Permite interrupción
+    // Interrupciones del ADC
     DIDR0 = 0;
-    ADMUX = 0x60;
-    ADCSRA = 0xEF;
+    ADMUX |= 1 << REFS0 | 1 << ADLAR;
+    ADCSRA |= 1 << ADIE | 1 << ADATE | 1 << ADSC | 1 << ADEN;
+    ADCSRA |= 1 << ADPS2 | 1 << ADPS1 | 1 << ADPS0;
     sei();
   }
-  // Infinite loop
+  // Ciclo infinito
   while (1)
-    // Game ON!
+    // Pelota en juegao
     while (inc) {
-      // Validate if someone won the game
+      // Valida si alguien gano
       if (inc && (PORTC & 0x3F) == 0) {
-        inc == 1 ? scoreB++ : scoreA++; // Increment game winner
-        if (scoreA == 4 || scoreB == 4) // Match winner ?
-          celebration(inc);             // Celebrate!
-        inc = PORTC = 0;                // Reset
+        inc == 1 ? scoreB++ : scoreA++; // Incrementa el ganador
+        if (scoreA == 4 || scoreB == 4) // Verifica si alguien gano la partida
+          resetPort(inc);               // Reset scores
+        inc = PORTC = 0;                // Reset quien tiene la pelota
+        TCCR1B &= ~(1 << CS12 | 1 << CS10);
+        // Configura para activar la interrupción del ADC
+        ADCSRA |= 1 << ADIE | 1 << ADATE | 1 << ADSC;
       }
-      // Displaying outputs (only need two bits from each number..)
-      PORTA = scoreA & 0x03;
-      PORTA <<= 2;
-      PORTA |= scoreB & 0x03;
+      // Desplegando salidas (Sólo dos bits de cada número)
+      PORTA = (scoreA << 2) & 0b1100 | (scoreB & 0b0011);
     }
 
-  return 0; // Never gets here but ... old traditions
+  return 0;
 }
 
-// Handle player moves
+// Movimientos del jugador A
 ISR(INT0_vect) {
   if (inc)
-    inc = (PORTC == 0x01 || PORTC == 0) ? -1 : inc;
+    inc = (PORTC == (1 << PC0) || PORTC == 0) ? -1 : inc;
   else {
-    PORTC = 0x01;
+    PORTC = 1 << PC0;
     inc = -1;
+    // Desactiva si la pelota esta en juego
+    ADCSRA &= ~(1 << ADIE | 1 << ADATE | 1 << ADSC);
+    TCCR1B |= 1 << CS12 | 1 << CS10;
   }
 }
 
-// Handle player moves
+// Movimientos del jugador B
 ISR(INT1_vect) {
   if (inc)
-    inc = (PORTC == 0x20 || PORTC == 0) ? 1 : inc;
+    inc = (PORTC == (1 << PC5) || PORTC == 0) ? 1 : inc;
   else {
-    PORTC = 0x20;
+    PORTC = 1 << PC5;
     inc = 1;
+    // Desactiva si la pelota esta en juego
+    ADCSRA &= ~(1 << ADIE | 1 << ADATE | 1 << ADSC);
+    TCCR1B |= 1 << CS12 | 1 << CS10;
   }
 }
 
-// Moves the ball
+// Mueve la bola
 ISR(TIMER1_COMPA_vect) {
   if (inc == 1)
-    // Player A to Player B
+    // Jugador A a Jugador B
     PORTC >>= 1;
   else if (inc == -1)
-    // Player B to Player A
+    // Jugador B a Jugador A
     PORTC <<= 1;
-
 }
 
+// Cambia velocidad
 ISR(ADC_vect) {
-  OCR1AL = ADCH;
+  OCR1A = ADCH;
+  OCR1A <<= 6;
 }
